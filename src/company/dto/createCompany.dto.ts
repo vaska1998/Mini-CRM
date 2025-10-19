543abf37dccd287a7131758e5885679089cb385c
Vasyl <vaska1601@ukr.net>
2025-10-19 09:13:26 +0300

    docs(swagger): add description to the company's interfaces for swagger

--- src/company/dto/createCompany.dto.ts
+++ src/company/dto/createCompany.dto.ts
@@ -1,14 +1,35 @@
-import { IsEmail, IsOptional, IsString, IsUrl } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+} from 'class-validator';
+import { ApiProperty, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Request')
 export class CreateCompanyDto {
+  @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: 'Name of company',
+    default: 'Company Name',
+  })
   name!: string;
 
   @IsOptional()
   @IsEmail()
+  @ApiProperty({
+    description: 'Email of company',
+    default: 'company@test.com',
+  })
   email?: string;
 
   @IsOptional()
   @IsUrl()
+  @ApiProperty({
+    description: 'Website of company',
+    default: 'https://test.com',
+  })
   website?: string;
 }
--- src/company/dto/updateCompany.dto.ts
+++ src/company/dto/updateCompany.dto.ts
@@ -1,15 +1,28 @@
 import { IsEmail, IsOptional, IsString, IsUrl } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateCompanyDto {
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    description: 'Name of company',
+    default: 'Company Name',
+  })
   name?: string;
 
   @IsOptional()
   @IsEmail()
+  @ApiProperty({
+    description: 'Email of company',
+    default: 'company@test.com',
+  })
   email?: string;
 
   @IsOptional()
   @IsUrl()
+  @ApiProperty({
+    description: 'Website of company',
+    default: 'https://test.com',
+  })
   website?: string;
 }
