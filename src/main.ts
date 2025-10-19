0abb61b13c75d2e1dfa3689811b4c3e3ad1deda1
Vasyl <vaska1601@ukr.net>
2025-10-19 09:04:28 +0300

    docs(swagger): add Swagger UI and API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService: ConfigService = app.get(ConfigService);
+  const port: number = Number(configService.get('APP_PORT'));
+
+  if (['local', 'dev'].includes(<string>configService.get('ENV'))) {
+    const config = new DocumentBuilder()
+      .setTitle('Mini-CRM API Doc')
+      .setDescription('API Documentation for Mini-CRM')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
+    console.log('Swagger Live');
+  }
+
+  await app.listen(port).then(() => console.log(`Listening on port ${port}`));
 }
 bootstrap();
